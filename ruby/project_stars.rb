917b3f722017de0c4ecf0c53f428e30d8a16b884
Philipp Leitner <leitner@infosys.tuwien.ac.at>
2020-12-10 16:08:58 +0100

    Scripts to fetch project metadata from GH plus initial results

---
+++ ruby/project_stars.rb
@@ -0,0 +1,156 @@
+require 'octokit'
+require 'time'
+
+class ProjectList
+    include Enumerable
+
+    attr_accessor :projects
+
+    def initialize
+        @projects = []
+    end
+
+    def self.parse(file)
+        list = ProjectList.new
+        File.open(file, 'r').each_line do |line|
+            org, p = line.split(/_/, 2)
+            next unless p   # if the splitting didn't work (e.g., in the file header) let's just skip this line
+            p.strip!
+            list.projects << Project.new(org, p)
+        end
+        return list
+    end
+
+    def each(&block)
+        @projects.each(&block)
+    end
+
+    def +(list)
+        @projects += list.projects
+        clearDuplicates
+    end
+
+    def sort!
+        cleanUnfetched
+        @projects.sort! { |a,b| b.metadata.watchers <=> a.metadata.watchers }
+    end
+
+    def print_to_file(file)
+        header = 'project;watchers;stargazers;subscribers;forks;is_forked'
+        File.open(file, 'w') do |f|
+            f.puts header
+            @projects.each{ |p| f.puts p.to_s }
+        end
+    end
+
+    def cleanUnfetched
+       @projects.filter!{ |p| p.metadata != nil } 
+    end
+
+    def clearDuplicates
+        @projects.uniq!(&:name)
+    end
+
+end
+
+class Project
+
+    attr_accessor :metadata
+
+    def initialize(org, name)
+        @org = org
+        @name = name
+    end
+
+    def name
+        @org+'/'+@name
+    end
+
+    def to_s
+        "#{name};#{@metadata.watchers};#{@metadata.stargazers};" +
+        "#{@metadata.subscribers};#{@metadata.forks};#{@metadata.is_forked}"
+    end
+
+end
+
+class ProjectMetadata
+
+    attr_accessor :watchers
+    attr_accessor :stargazers
+    attr_accessor :subscribers
+    attr_accessor :forks
+    attr_accessor :is_forked
+
+    def initialize(watchers, stargazers, subscribers, forks, forked)
+        @watchers = watchers
+        @stargazers = stargazers
+        @subscribers = subscribers
+        @forks = forks
+        @is_forked = forked
+    end
+
+end
+
+class ProjectFetcher
+
+    def initialize(key)
+        @client = Octokit::Client.new(:access_token => key)
+    end
+
+    def fetchProjectsForFile(file)
+        projects = ProjectList.parse(file)
+        projects.each do |project|
+            
+            checkRateLimit
+
+            # fetch repo
+            puts "Fetching #{project.name}"
+            begin
+                repo = @client.repo(project.name)
+                metadata = ProjectMetadata.new(repo.watchers_count, repo.stargazers_count, repo.subscribers_count, repo.forks, repo.fork)
+                project.metadata = metadata
+            rescue Octokit::TooManyRequests
+                checkRateLimit
+            rescue Octokit::NotFound
+                puts "Not found: #{project.name}"
+            rescue Octokit::InvalidRepository
+                puts "Invalid repo: #{project.name}"
+            rescue Octokit::RepositoryUnavailable
+                puts "Unavailable repo: #{project.name}"
+            rescue Octokit::UnavailableForLegalReasons
+                puts "Access denied: #{project.name}"
+            end
+
+        end
+        return projects
+    end
+
+    def checkRateLimit
+        if @client.rate_limit.remaining <= 0 then
+            puts "Rate limit reached, sleeping until #{@client.rate_limit.resets_at.to_s}"
+            sleep(@client.rate_limit.resets_at - Time.now + 60)
+        end
+    end
+
+end
+
+infile = ARGV[0]
+outfile = ARGV[1]
+keyIdx = ARGV[2]
+
+API_KEYS = [
+    '2ca6d9594922b23f15171e60ac65eeb4b2b429ac',
+    'bd9571580b46ed7e6b024a54eb53188a00f1a311',
+    'dba6c44d2468f262a756861b9db5a2f5b63939f1'
+]
+
+puts "Using in: #{infile} and out: #{outfile}"
+fetcher = ProjectFetcher.new(API_KEYS[keyIdx.to_i])
+puts "Fetching file #{infile}"
+projects = fetcher.fetchProjectsForFile(infile)
+
+puts "Done fetching. Sorting #{projects.count} projects."
+projects.sort!
+
+puts "Printing to #{outfile}."
+projects.print_to_file(outfile)
